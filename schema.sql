c6be73ff0747fe43c2f4912f365adf23000debb5
Maximilian Heymann <maxhey4@gmail.com>
2020-10-26 14:48:20 +0100

    add database schema

---
+++ schema.sql
@@ -0,0 +1,52 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.14 (Ubuntu 10.14-0ubuntu0.18.04.1)
+-- Dumped by pg_dump version 10.14 (Ubuntu 10.14-0ubuntu0.18.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: items; Type: TABLE; Schema: public; Owner: idleapi
+--
+
+CREATE TABLE public.items (
+    "user" bigint NOT NULL,
+    protected bigint[] DEFAULT '{}'::bigint[]
+);
+
+
+ALTER TABLE public.items OWNER TO idleapi;
+
+--
+-- PostgreSQL database dump complete
+--
+
